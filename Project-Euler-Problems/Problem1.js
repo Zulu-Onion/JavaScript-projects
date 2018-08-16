366e67d11a3055bbfb2601ed0047f9f9031b9595
Zulu-Onion <40836609+Zulu-Onion@users.noreply.github.com>
2018-08-16 08:21:56 -0400

    Update Problem1.js

--- Project-Euler-Problems/Problem1.js
+++ Project-Euler-Problems/Problem1.js
@@ -8,7 +8,7 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 function projectEuler1(){
     let values = [];
     const reducer = (accumulator, currentValue) => accumulator + currentValue;
-    for (let num = 1; num <= 999; num++) {
+    for (let num = 1; num < 1000; num++) {
         if (num % 3 === 0 || num % 5 === 0){
             values.push(num);
         }
